4a9e3810cb9e19306b0da1e0479571d641d327a0
joshlong <josh@joshlong.com>
2018-07-23 12:18:15 -0700

    initial commit of the bootcampl code.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# bootcamp
-start here! Let's review some basics before we dive into security.
+# Bootcamp 
+
+* Spring Boot Bootcamp: Initializr in the browser and in the IDE
+* Things You’ll Need: Java 8, an IDE, Maven, Spring Boot CLI
+* Spring Introduction: Dependency Injection
+* SpEL
+* AOP 
+* How Servlet Filter Works: Talk about how to implement FilterChain and how Filter is like AOP
---
+++ src/main/java/com/example/intro/IntroApplication.java
@@ -0,0 +1,144 @@
+package com.example.intro;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.UUID;
+
+@SpringBootApplication
+public class IntroApplication {
+
+		@Aspect
+		@Component
+		public static class ServiceMonitor {
+
+				private final Log log = LogFactory.getLog(getClass());
+
+				@Around("execution(* com.example..*.*(..))")
+				public Object log(ProceedingJoinPoint joinPoint) {
+						try {
+								String s = joinPoint.toString();
+								Object result = joinPoint.proceed();
+								log.info("finished " + s + ".");
+								return result;
+						}
+						catch (Throwable throwable) {
+								throw new RuntimeException(throwable);
+						}
+				}
+		}
+
+		@Bean
+		Bar bar() {
+				return new Bar();
+		}
+
+		@Bean
+		Foo foo(Bar bar) {
+				return new Foo(bar);
+		}
+
+		@Bean
+		ApplicationRunner uuidRunner(@Value("#{ up.buildUUID() } ") String uuid,
+																															@Value("#{ 2 < 1 }") boolean simpleMaths) {
+				Log log = LogFactory.getLog(getClass());
+				return args -> {
+						log.info("UUID: " + uuid);
+						log.info("simple maths: " + simpleMaths);
+				};
+		}
+
+		@Component("up")
+		public static class UUIDProducer {
+
+				public String buildUUID() {
+						return UUID.randomUUID().toString();
+				}
+		}
+
+		@Bean
+		ApplicationRunner barRunner(Bar bar) {
+				return args -> bar.hi();
+		}
+
+		@RestController
+		public static class GreetingsRestController {
+
+				@GetMapping("/hi/{name}")
+				String name(@PathVariable String name) {
+						return "hello, " + name + "!";
+				}
+		}
+
+		@Component
+		public static class SimpleFilter implements javax.servlet.Filter {
+
+				private final Log log = LogFactory.getLog(getClass());
+
+				@Override
+				public void init(FilterConfig filterConfig) {
+						this.log.info("init(filterConfig)");
+				}
+
+				@Override
+				public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+						this.log.info("before doFilter(request,response,chain)");
+						Assert.isTrue(request instanceof HttpServletRequest, "this must be an " + HttpServletRequest.class.getName() + ".");
+						HttpServletRequest httpServletRequest = HttpServletRequest.class.cast(request);
+						log.info("URI: " + httpServletRequest.getRequestURI());
+						String parm;
+						if ((parm = request.getParameter("allow")) != null && Boolean.parseBoolean(parm)) {
+								this.log.info("welcome!");
+								chain.doFilter(request, response);
+						}
+						else {
+								this.log.info("denied!");
+						}
+						this.log.info("after doFilter(request,response,chain)");
+				}
+
+				@Override
+				public void destroy() {
+						this.log.info("destroy()");
+				}
+		}
+
+		public static void main(String[] args) {
+				SpringApplication.run(IntroApplication.class, args);
+		}
+}
+
+
+class Foo {
+		private final Bar bar;
+
+		Foo(Bar bar) {
+				this.bar = bar;
+		}
+}
+
+class Bar {
+
+		private final Log log = LogFactory.getLog(getClass());
+
+		public void hi() {
+				log.info("#hi()");
+		}
+}
+
