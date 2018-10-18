013aae9e9de49ae84deb8a31373d96953ddd9ee9
Josh Long <josh@joshlong.com>
2018-10-19 02:32:16 +0300

    polish

--- src/main/java/com/example/bootcamp/UuidService.java
+++ src/main/java/com/example/bootcamp/UuidService.java
@@ -4,9 +4,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
-/**
- * @author <a href="mailto:josh@joshlong.com">Josh Long</a>
- */
 @Component("uuid")
 class UuidService {
 
